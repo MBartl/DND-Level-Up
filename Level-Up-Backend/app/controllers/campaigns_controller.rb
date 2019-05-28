c7028276ad7f6316f9580b27ed9415e85219dd49
github username <github email address>
2019-05-28 12:53:56 -0400

    Attempting to create some seed data for diagnostics.

--- Level-Up-Backend/app/controllers/campaigns_controller.rb
+++ Level-Up-Backend/app/controllers/campaigns_controller.rb
@@ -8,9 +8,15 @@ class CampaignsController < ApplicationController
   def create
     @campaign = Campaign.new(campaign_params)
     if @campaign.save
-      redirect_to @campaign
+      render json: @campaign
     else
       render :new
     end
   end
+end
+
+private
+
+def campaign_params
+  params.require(:campaign).permit(:name, :plot_notes)
 end
